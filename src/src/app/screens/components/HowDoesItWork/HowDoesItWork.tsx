0b5b4f9b49085fc51f2f273992f4bfa3b2116148
azernn <127416294+azernn@users.noreply.github.com>
2023-04-27 06:24:18 -0700

    Add files via upload

---
+++ src/src/app/screens/components/HowDoesItWork/HowDoesItWork.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/style-prop-object */
+/* eslint-disable jsx-a11y/alt-text */
+<div className="HowDoesItWork">
+            <div className="main-title">How does it work</div>
+            <div className="features">
+                <div className="frame1">
+                    <div className="icon"><img src="Icons (1).png" style="width: 168px;"/></div>
+                    <div className="texts">
+                        <div className="title">Select location</div>
+                        <div className="body">Choose the location where your food will be delivered.</div>
+                    </div>
+                </div>
+                <div className="frame1">
+                    <div className="icon"> <img src="Icons (2).png" style="width: 168px;"/></div>
+                    <div className="texts">
+                        <div className="title">Choose order</div>
+                        <div className="body">Check over hundreds of menus to pick your favorite food</div>
+                    </div>
+                </div>
+                <div className="frame1">
+                    <div className="icon"> <img src="Icons (3).png" style="width: 160px;"/></div>
+                    <div className="texts">
+                        <div className="title">Pay advanced</div>
+                        <div className="body">It's quick, safe, and simple. Select several methods of payment</div>
+                    </div>
+                </div>
+                <div className="frame1">
+                    <div className="icon"> <img src="Icons (4).png" style="width: 168px;"/></div>
+                    <div className="texts">
+                        <div className="title">Enjoy meals</div>
+                        <div className="body">Food is made and delivered directly to your home.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
